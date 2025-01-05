2c31b0b302ba52e570e2917eaed5c1a657ef0743
DylM0nster22 <tetherpro@gmail.com>
2025-01-05 14:53:42 -0800

    added the rest of the things from the vercel

--- js/multiplayer.js
+++ js/multiplayer.js
@@ -11,9 +11,9 @@ class MultiplayerManager {
     }
 
     initializeSocket() {
-        this.socket = new WebSocket('wss://your-vercel-deployment.vercel.app');
+        this.socket = new WebSocket('wss://top-down-shooter-99s3gsqh1-dylm0nster22s-projects.vercel.app');
         this.setupSocketListeners();
-    }
+    }    
 
     createRoom() {
         this.socket.send(JSON.stringify({
@@ -28,6 +28,23 @@ class MultiplayerManager {
             roomCode: code,
             skin: this.selectedSkin
         }));
+    }
+
+    setupSocketListeners() {
+        this.socket.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            switch(data.type) {
+                case 'room_created':
+                    this.roomCode = data.roomCode;
+                    break;
+                case 'game_start':
+                    game.startGame();
+                    break;
+                case 'player_moved':
+                    game.updateOtherPlayerPosition(data.x, data.y);
+                    break;
+            }
+        };
     }
 
     updatePlayerPosition(x, y) {
